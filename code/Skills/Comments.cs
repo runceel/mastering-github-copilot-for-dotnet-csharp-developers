a3565e8f51df74ae40f4dfa3b524cee5bca67ac9
Kazuki Ota <kaota@microsoft.com>
2025-03-03 05:55:53 +0000

    Add initial project files and implement CalculateNumbers function

---
+++ code/Skills/Comments.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Skills;
+
+public class Comments
+{
+    // create a method that generates a comment for a given skill
+    public string GenerateComment(string skill)
+    {
+        return $"You have a great skill in {skill}!";
+    }
+}
