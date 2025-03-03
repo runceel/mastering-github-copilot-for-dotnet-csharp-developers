c924496e3338f6b4931ff04e6b3e7658b2db0e77
Kazuki Ota <kaota@microsoft.com>
2025-03-03 05:52:35 +0000

    Added Member.cs file

---
+++ code/Skills/Member.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Skills;
+
+public class Member
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+}
